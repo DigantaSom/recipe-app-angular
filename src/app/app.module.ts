3de82ef8aa2d7bf1f56010b65833776ebc5f0752
Diganta Som <itsdsom111@gmail.com>
2022-02-14 00:49:56 +0530

    Adding new-recipe and edit-recipe routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown-directive/dropdown.directive';
@@ -23,6 +24,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
+    RecipeEditComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
