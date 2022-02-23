d8b49f0a99f8e55de4cb2296387236f108a4e391
Diganta Som <itsdsom111@gmail.com>
2022-02-23 12:45:39 +0530

    Moved the dropdown-directive from its own folder to outside (i.e. in the 'shared' folder)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/dropdown-directive/dropdown.directive';
+import { DropdownDirective } from './shared/dropdown.directive';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 @NgModule({
