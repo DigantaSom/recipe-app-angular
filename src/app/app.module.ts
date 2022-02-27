0c411a7c88ee6b2f4d671b77db037c6c70d7cbc1
Diganta Som <itsdsom111@gmail.com>
2022-02-27 21:55:28 +0530

    Outsourcing the Service(s) provided in the AppModule to their own core-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
-
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { CoreModule } from './core.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -25,13 +24,7 @@ import { AuthComponent } from './auth/auth.component';
     RecipesModule,
     ShoppingListModule,
     SharedModule,
-  ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true,
-    },
+    CoreModule,
   ],
   bootstrap: [AppComponent],
 })
