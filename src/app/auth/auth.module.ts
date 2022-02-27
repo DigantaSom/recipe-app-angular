aa56858e6f332f36916ac96e9dd2740150f518f2
Diganta Som <itsdsom111@gmail.com>
2022-02-27 22:13:18 +0530

    Outsourcing the auth related things to their own feature-module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { AuthComponent } from './auth.component';
+
+const authRoutes: Routes = [{ path: 'auth', component: AuthComponent }];
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(authRoutes),
+    SharedModule,
+  ],
+})
+export class AuthModule {}
